a6b8281aa89543981c7ec6d5b301f4aeb6874794
Angeline <siv.angeline@ymail.com>
2019-02-05 22:22:19 -0500

    + Added dummy Projectile
    + Created "Projectiles" Assets folder
    + Uploaded projectile_test sprite
    + Added RigidBody2D and CircleCollider2D on projectile_test
    + Added script "ArmTrebuchet"

---
+++ Team Game Project/Assets/Scripts/ArmTrebuchet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmTrebuchet : MonoBehaviour
+{
+    private Collider2D obj;
+
+    public void Start()
+    {
+        obj.GetComponent<Collider2D>();
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void OnCollisionEnter()
+    {
+        
+    }
+}
