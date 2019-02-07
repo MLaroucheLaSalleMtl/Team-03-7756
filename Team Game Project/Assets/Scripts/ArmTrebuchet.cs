425631b22355489641647f50b165d003def41c1d
Angeline <siv.angeline@ymail.com>
2019-02-06 20:14:55 -0500

    + Renamed gameobject, folders names and modified Rigidbody2D behaviours
    + Created new layer "Projectile" and disable that collision with Character
    - Removed original Trebuchet sprites from Assets folder
    - Removed TemporaryFloor gameobject from the scene
    + Created new folder "Character" and moved the player ssprite in it
    + Modified collision detection to continuous for parts of the Trebuchet

--- Team Game Project/Assets/Scripts/ArmTrebuchet.cs
+++ Team Game Project/Assets/Scripts/ArmTrebuchet.cs
@@ -6,7 +6,7 @@ public class ArmTrebuchet : MonoBehaviour
 {
     public void OnCollisionEnter(Collision col)
     {
-        if (col.gameObject.name == "sling_2")
+        if (col.gameObject.name == "sling")
         {
             Debug.Log("Collided!");
             gameObject.transform.position = col.gameObject.transform.position;
