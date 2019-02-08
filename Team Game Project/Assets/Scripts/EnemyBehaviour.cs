a1a15c1d509ea35818a0ba7b1f479a55f82ce61b
Christian Anton Sladeczek <chris1vid@gmail.com>
2019-02-08 14:10:25 -0500

    added in enemy and spawner

---
+++ Team Game Project/Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+
+    public GameObject player;
+    float speed = 2;
+    float x;
+    float y;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+
+
+    void Move()
+    {
+        gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, player.transform.position, Time.deltaTime * speed);
+    }
+}
+
---
+++ Team Game Project/Assets/Scripts/Spawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    public Transform[] spawnPointArray;
+    public GameObject testEnemyPrefab;
+
+    float spawnRate = 5f;      //time between each spawn. this will later be made dynamic to increase spawn rates
+    float startSpawnAt = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnTest", startSpawnAt, spawnRate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    void SpawnTest()
+    {
+        // later include methods to make spawnrate decrease over time.time
+
+        //int spawnPointIndex = Random.Range(0, spawnPointArray.Length); // implement something like this later for multiple spawns
+
+        Instantiate(testEnemyPrefab, spawnPointArray[0].position, spawnPointArray[0].rotation);
+    }
+}
