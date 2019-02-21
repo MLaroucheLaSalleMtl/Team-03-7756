fa03dc94079a74ae87c8a6f289b97c2e44622aac
Angeline <siv.angeline@ymail.com>
2019-02-21 01:29:54 -0500

    Updated FarmSystem
    + Created Sprites>Crops Folder and addded seeds_test sprite
    + Added RigidBody and Collider on seeds_test and created prefab
    + Added spawners for crops
    + Created GrowCrop script: seeds touch the spawner on the field they instantiate new projectiles

---
+++ Team Game Project/Assets/Scripts/Game/GrowCrop.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrowCrop : MonoBehaviour
+{
+    [SerializeField] private GameObject prefabProjectile;
+    
+    public void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Seed")
+        {
+            Debug.Log("Collided!");
+            Destroy(col.gameObject);
+            Instantiate(prefabProjectile, transform.position, transform.rotation);
+        }
+    }
+}
