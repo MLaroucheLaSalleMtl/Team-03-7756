e6e31cf56c74f9b69a7cc7fd84e5fa930fff0e19
Sarah <sttauf@yahoo.com.br>
2019-02-21 09:07:27 -0500

    + Added the PanelPause script
    - I added a GameManager and finished the scripts for the ManuPause

---
+++ Team Game Project/Assets/Scripts/UI/MenuPauseScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPauseScript : MonoBehaviour
+{
+    public GameObject panelPause;
+    public static bool isPaused = false;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused) { Resume(); } else { Pause(); }
+        }
+        
+    }
+    public void Pause()
+    {
+        panelPause.SetActive(true);
+        isPaused = true;
+        Time.timeScale = 0f;
+    }
+    public void Resume()
+    {
+        panelPause.SetActive(false);
+        isPaused = false;
+        Time.timeScale = 1f;
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("mainmenu");
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
