b8cd5cab3f03a2c5ff61d22d5d91003d074ecfb4
Christian Anton Sladeczek <chris1vid@gmail.com>
2019-03-05 17:10:24 -0500

    Created test sling & anchor

--- Team Game Project/Assets/Scripts/Game/ClickDrag.cs
+++ Team Game Project/Assets/Scripts/Game/ClickDrag.cs
@@ -5,25 +5,59 @@ using UnityEngine;
 // Source: https://stackoverflow.com/questions/23152525/drag-object-in-unity-2d
 
 public class ClickDrag : MonoBehaviour
-{   
+{
     /// <summary>
     /// To refine
     /// </summary>
     private Vector3 screenPoint;
     private Vector3 offset;
+    public Rigidbody2D rigidBody;
+
+    private float releaseDelay = 0.1f;
+
+    private bool dragging = false;
+
+    private void Update()
+    {
+        if (dragging == true)
+        {
+            rigidBody.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+    }
 
     public void OnMouseDown()
     {
-        offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+        dragging = true;
+        rigidBody.isKinematic = true;
+        //offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
     }
 
     /// <summary>
     /// Allows for the object to move according to mouse click and drag
     /// </summary>
-    public void OnMouseDrag()
+    /*public void OnMouseDrag()
     {
         Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
         Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
         transform.position = curPosition;
+    }*/
+
+    public void OnMouseUp()
+    {
+        dragging = false;
+        rigidBody.isKinematic = false;
+
+        StartCoroutine(
+            
+            Release()
+            
+            ); 
+    }
+
+    IEnumerator Release ()
+    {
+        yield return new WaitForSeconds(releaseDelay);
+
+        GetComponent<SpringJoint2D>().enabled = false;
     }
 }
