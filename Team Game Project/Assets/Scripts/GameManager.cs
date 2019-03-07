2d588b02a3939f8e930b59e8ddd870d39179d79c
Angeline <siv.angeline@ymail.com>
2019-03-07 04:54:27 -0500

    + Added GetScore script
    + Updated Scoreboard
    + Update GameManager

--- Team Game Project/Assets/Scripts/GameManager.cs
+++ Team Game Project/Assets/Scripts/GameManager.cs
@@ -6,7 +6,8 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private Text pointText;
-    private int totalPoints;
+    public int totalPoints;
+    public int highestScore;
     [SerializeField] private Text timerText;
     private float timerCountDown = 90.0f;
 
@@ -24,16 +25,42 @@ public class GameManager : MonoBehaviour
         timerText.text = timerCountDown.ToString("0:00");
         pointText.text = "SCORE: " + totalPoints.ToString("D4");
 
-        if(timerCountDown <= 0.0f)
+        if (timerCountDown <= 0.0f)
         {
             Time.timeScale = 0.0f;
             //LEVEL OVER
             endGame.SetActive(true);
+
+            //Keep track of score
+            EnterName();
+            SaveScore();
         }
     }
 
     public void AddPoints()
     {
         totalPoints += 25;
+    }
+
+    public void SaveScore()
+    {
+        if (totalPoints > PlayerPrefs.GetInt("HighestScore"))
+        {
+            highestScore = totalPoints;
+            PlayerPrefs.SetInt("HighestScore", highestScore);
+            PlayerPrefs.Save();
+        }
+    }
+
+    [SerializeField] private InputField playerName;
+
+    public void EnterName()
+    {
+        if (Input.GetKeyDown("Enter"))
+        {
+            PlayerPrefs.SetString("PlayerName", playerName.text);
+            PlayerPrefs.SetInt("PlayerScore", totalPoints);
+            PlayerPrefs.Save();
+        }
     }
 }
---
+++ Team Game Project/Assets/Scripts/UI/GetScore.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GetScore : MonoBehaviour
+{
+    private GameManager gameManager;
+    [SerializeField] private Text highestScore;
+
+    public void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+        PlayerPrefs.GetInt("HighestScore");
+
+        //DISPLAY SCOREBOARD
+        highestScore.text = "Best Peasant: " + PlayerPrefs.GetString("HighestName") + "     " + PlayerPrefs.GetInt("HighestScore");
+
+    }
+}
