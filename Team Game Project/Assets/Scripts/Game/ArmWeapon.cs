2c2adf91aad5db30ad4b7e8403471747daf4034d
Angeline <siv.angeline@ymail.com>
2019-03-06 23:06:41 -0500

    + Added ArmWeapon script on BallistaPivot
    + Updated Ballista gameobject collider
    + Added Sprint Joint 2D to projectile prefab
    + Updated ArmWeapon script

---
+++ Team Game Project/Assets/Scripts/Game/ArmWeapon.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmWeapon : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D ballistaAnchor;
+
+    public void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Projectile")
+        {
+            Debug.Log("Collided!");
+
+            //Activate the Spring Joint 2D on projectile and attach it to the anchor
+            col.gameObject.GetComponent<SpringJoint2D>().enabled = true;
+            col.gameObject.GetComponent<SpringJoint2D>().connectedBody = ballistaAnchor;
+
+            //Disable gravity on projectile
+            col.gameObject.GetComponent<Rigidbody2D>().gravityScale = 0.0f;
+        }
+    }
+}
