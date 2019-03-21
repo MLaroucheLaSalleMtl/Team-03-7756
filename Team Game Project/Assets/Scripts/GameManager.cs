3dee0a79b2877d01bb6389d40409a91176591f88
Sarah <sttauf@yahoo.com.br>
2019-03-21 11:16:55 -0400

    + Added Scripts and Panel for the Score System - NOT TESTED
    - I deleted the GetScore() script since I add this function on GameManager, I also created a class called Score(), where is just declared the name and score.
    - Inside of GameManager I added SaveScore(0 where it checks if the score is bigger than the highest score and if yes open the new panel so the player can add his/her name and be added to the Score list.
    - I didn't tested yet, but I'm posting it so you can check and understand how it works (I added comments to understand the logic in case of doesn't work).

--- Team Game Project/Assets/Scripts/GameManager.cs
+++ Team Game Project/Assets/Scripts/GameManager.cs
@@ -5,15 +5,23 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    //Level Manager
     [SerializeField] private Text pointText;
-    public int totalPoints;
     public int highestScore;
     [SerializeField] private Text timerText;
     private float timerCountDown = 90.0f;
     private EnemyBehaviour enemy;
-
     [SerializeField] private GameObject endGame;
 
+    //ScoreBoard System
+    public int totalPoints; //all the points that the player got
+    private const int scoreListLength = 20; //Amount of players on the score list
+    List<Score> ScoreBoardList = new List<Score>(); //score list
+    [SerializeField] private GameObject leaderboardNameInput; //Panel that'll be activate when the player reach any of the top20 scores, so he/she can add their names
+    [SerializeField] private InputField playerName; //The input field
+    
+    
+    //Level Manager
     public void Start()
     {
         Time.timeScale = 1.0f;
@@ -33,11 +41,11 @@ public class GameManager : MonoBehaviour
             endGame.SetActive(true);
 
             //Keep track of score
-            EnterName();
             SaveScore();
         }
     }
 
+    //ScoreBoard System
     public void AddPoints()
     {
         totalPoints += enemy.enemyPoints;
@@ -45,23 +53,72 @@ public class GameManager : MonoBehaviour
 
     public void SaveScore()
     {
-        if (totalPoints > PlayerPrefs.GetInt("HighestScore"))
-        {
-            highestScore = totalPoints;
-            PlayerPrefs.SetInt("HighestScore", highestScore);
-            PlayerPrefs.Save();
-        }
+                //GetScore
+                //int j = 1;
+                //do
+                //{
+                //    Score playerInfo = new Score();
+                //    playerInfo.PlayerName = PlayerPrefs.GetString("HighScore" + j + "playerName"); //Add the name
+                //    playerInfo.PlayerScore = PlayerPrefs.GetInt("HighScore" + j + "playerScore");  //And the score to playerprefs
+                //    ScoreBoardList.Add(playerInfo); //And to the list
+                //    j++;
+                //} while (j <= scoreListLength && PlayerPrefs.HasKey("HighScore" + j + "playerScore"));
+
+
+                //If there is nothing on the score list
+                if (ScoreBoardList.Count == 0)
+                {
+                    leaderboardNameInput.SetActive(true); //The player can add his/her name as the first high score
+                    EnterName();
+                    Score playerInfo = new Score();
+                    playerInfo.PlayerName = PlayerPrefs.GetString("PlayerName" + "playerName");
+                    playerInfo.PlayerScore = PlayerPrefs.GetInt("PlayerScore" + "playerScore"); //Take the info
+                    ScoreBoardList.Add(playerInfo); //Add the info to the list
+                }
+                //If there is anything on the score list
+                else
+                {
+                    for (int k = 1; k <= ScoreBoardList.Count && k <= scoreListLength; k++)
+                    {
+                        if (totalPoints > ScoreBoardList[k - 1].PlayerScore) //Check if the score is bigger than any score on the top20 list
+                        {
+                            leaderboardNameInput.SetActive(true); //If it is, the player can add his/her name
+                            EnterName(); 
+                            Score playerInfo = new Score();
+                            playerInfo.PlayerName = PlayerPrefs.GetString("HighScore" + "playerName");
+                            playerInfo.PlayerScore = PlayerPrefs.GetInt("HighScore" + "playerScore"); //Take the info
+                            ScoreBoardList.Insert(k - 1, playerInfo); //Add to the list
+                            break;
+                        }
+                        if (k == ScoreBoardList.Count && k < scoreListLength) //Check if there is 20 players on the list, in case of the player score be smaller than all other players but there is space on the list without score 
+                        {
+                            leaderboardNameInput.SetActive(true); //If it is, the player can add his/her name
+                            EnterName();
+                            Score playerInfo = new Score();
+                            playerInfo.PlayerName = PlayerPrefs.GetString("HighScore" + "playerName");
+                            playerInfo.PlayerScore = PlayerPrefs.GetInt("HighScore" + "playerScore"); //Take the info
+                            ScoreBoardList.Add(playerInfo); //Add info to the list
+                            break;
+                        }
+                    }
+                }
+                //int i = 1;
+                //do
+                //{
+                //    PlayerPrefs.SetString("HighScore" + i + "playerName", ScoreBoardList[i - 1].PlayerName);
+                //    PlayerPrefs.SetInt("HighScore" + i + "playerScore", ScoreBoardList[i - 1].PlayerScore);
+                //    i++;
+                //} while (scoreListLength <= i && ScoreBoardList.Count <= i);
+                   
+                PlayerPrefs.Save(); //Save
     }
-
-    [SerializeField] private InputField playerName;
-
     public void EnterName()
     {
-        //if (Input.GetKeyDown("Enter"))
-        //{
-        //    PlayerPrefs.SetString("PlayerName", playerName.text);
-        //    PlayerPrefs.SetInt("PlayerScore", totalPoints);
-        //    PlayerPrefs.Save();
-        //}
+        if (Input.GetKeyDown("Enter"))
+        {
+            PlayerPrefs.SetString("PlayerName", playerName.text);
+            PlayerPrefs.SetInt("PlayerScore", totalPoints);
+            PlayerPrefs.Save();
+        }
     }
 }
---
+++ Team Game Project/Assets/Scripts/UI/Score.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+    {
+        private string playerName;
+        private int playerScore;
+        public string PlayerName { get => playerName; set => playerName = value; }
+        public int PlayerScore { get => playerScore; set => playerScore = value; }
+    }
