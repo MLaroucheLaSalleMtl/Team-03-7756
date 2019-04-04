208c15876b3a2fa9211221ab44cdbe46d90863ee
Angeline <siv.angeline@ymail.com>
2019-04-04 08:52:01 -0400

    Auto stash before merge of "master" and "origin/master"

--- Team Game Project/Assets/Scripts/Game/ClickAndLaunchObject.cs
+++ Team Game Project/Assets/Scripts/Game/ClickAndLaunchObject.cs
@@ -6,67 +6,70 @@ using UnityEngine;
 
 public class ClickAndLaunchObject : MonoBehaviour
 {
-    private Vector3 screenPoint;
-    private Vector3 offset;
-    private Vector2 prevVelocity;
-
     private float releaseDelay = 0.1f;
-    private bool isBeingDragged = true;
+    private bool isBeingDragged = false;
     private ArmWeapon weapon;
+
+    //private Vector2 prevVelocity;
 
     private void Awake()
     {
         weapon = FindObjectOfType<ArmWeapon>();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
-        if(GetComponent<SpringJoint2D>() != null) //if the spring joint exists
+        if (isBeingDragged)
         {
-            if (!GetComponent<Rigidbody2D>().isKinematic && prevVelocity.sqrMagnitude > GetComponent<Rigidbody2D>().velocity.sqrMagnitude)
-            {
-                weapon.isArmed = false;
-                Destroy(GetComponent<SpringJoint2D>());
-                GetComponent<Rigidbody2D>().velocity = prevVelocity;
-            }
-            if(!isBeingDragged)
-            {
-                prevVelocity = GetComponent<Rigidbody2D>().velocity;
-            }
+            GetComponent<Rigidbody2D>().position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         }
+
+        if (weapon.isArmed && isBeingDragged)
+        {
+            StartCoroutine(LaunchProjectile());
+        }
+
+        //if(GetComponent<SpringJoint2D>() != null) //if the spring joint exists
+        //{
+        //    if (!GetComponent<Rigidbody2D>().isKinematic && prevVelocity.sqrMagnitude > GetComponent<Rigidbody2D>().velocity.sqrMagnitude)
+        //    {
+        //        weapon.isArmed = false;
+        //        Destroy(GetComponent<SpringJoint2D>());
+        //        GetComponent<Rigidbody2D>().velocity = prevVelocity;
+        //    }
+        //    if(!isBeingDragged)
+        //    {
+        //        prevVelocity = GetComponent<Rigidbody2D>().velocity;
+        //    }
+        //}
     }
 
     public void OnMouseDown()
     {
-        offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
-        GetComponent<SpringJoint2D>().enabled = false;
-
-        if (weapon.isArmed)
-        {
-            isBeingDragged = true;
-        }
+        isBeingDragged = true;
         GetComponent<Rigidbody2D>().isKinematic = true;
+
+        //offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+        //GetComponent<SpringJoint2D>().enabled = false;
     }
 
     public void OnMouseUp()
     {
-        GetComponent<SpringJoint2D>().enabled = true;
         isBeingDragged = false;
-        GetComponent<Rigidbody2D>().isKinematic = false; 
+        GetComponent<Rigidbody2D>().isKinematic = false;
+        //GetComponent<SpringJoint2D>().enabled = true;
+
+        if (GetComponent<SpringJoint2D>() != null)
+        {
+            StartCoroutine(LaunchProjectile());
+        }
     }
 
-    public void OnMouseDrag()
-    {
-        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
-        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
-        transform.position = curPosition;
-    }
-
-    IEnumerator Release()
+    IEnumerator LaunchProjectile()
     {
         yield return new WaitForSeconds(releaseDelay);
         GetComponent<SpringJoint2D>().enabled = false;
-        GetComponent<SpringJoint2D>().connectedBody = null;
+        //GetComponent<SpringJoint2D>().connectedBody = null;
         weapon.isArmed = false;
     }
 }
---
+++ Team Game Project/Assets/Scripts/Game/LaunchObject.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaunchObject : MonoBehaviour
+{
+    private float releaseTime = 0.1f;
+    private bool isClicked = false;
+    private ArmWeapon weapon;
+
+    public void Awake()
+    {
+        weapon = FindObjectOfType<ArmWeapon>();
+    }
+
+    public void Update()
+    {
+        if (isClicked)
+        {
+            GetComponent<Rigidbody2D>().position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+    }
+
+    public void OnMouseDown()
+    {
+        isClicked = true;
+        GetComponent<Rigidbody2D>().isKinematic = true;
+        GetComponent<Rigidbody2D>().simulated = true;
+    }
+
+    public void OnMouseUp()
+    {
+        isClicked = false;
+        GetComponent<Rigidbody2D>().isKinematic = false;
+
+        StartCoroutine(Launch());
+    }
+
+    IEnumerator Launch()
+    {
+        yield return new WaitForSeconds(releaseTime);
+
+        GetComponent<SpringJoint2D>().enabled = false;
+        weapon.isArmed = false;
+    }
+}
+
