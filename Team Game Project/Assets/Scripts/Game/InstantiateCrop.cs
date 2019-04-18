f5613a424186a4084f6a526e8247cf497e9611f2
Angeline <siv.angeline@ymail.com>
2019-04-18 00:43:19 -0400

    - Removed GrowCrop script
    + Added InstantiateCrop script and attached it to seed
    + Made the FarmSystem take account for future/different crop instantiation

---
+++ Team Game Project/Assets/Scripts/Game/InstantiateCrop.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateCrop : MonoBehaviour
+{
+    [SerializeField] private GameObject[] Crops;
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Planteable Area")
+        {
+            Debug.Log("collided");
+            Destroy(gameObject);
+            Instantiate(Crops[Random.Range(0, Crops.Length)], collision.gameObject.transform.position, collision.gameObject.transform.rotation);
+        }
+    }
+}
